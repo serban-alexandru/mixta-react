de2f9e2532f7c4acecda54c76ab798cdc7e6f0cd
Serban Alexandru <serbann.alexandruu@yahoo.com>
2020-06-06 15:49:24 +0300

    Mixta react app

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const data = {
-  //   baseUrl: "http://burgery.online",
-  baseUrl: "http://localhost/burgery/public",
+  baseUrl: "http://burgery.online",
+  // baseUrl: "http://localhost/burgery/public",
 };
 
 export default data;
